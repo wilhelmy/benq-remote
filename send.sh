1c193da6a164ffbedb23dcd6bebe389b1bf4a110
Andreas Demmelbauer <git@notice.at>
2017-06-24 15:20:29 +0200

    add command line mode

--- send.sh
+++ send.sh
@@ -2,6 +2,20 @@
 # See the "COPYING" file distributed along with this script for the MIT/X
 # Consortium License.
 
+IP=10.0.0.118
+PORT=41794
+
+if [ "$1" ]; then
+	if [ -f "$1" ]; then
+		echo "00: `< "$1"`" | xxd -r | nc -w 2 $IP $PORT | xxd
+		exit 0
+	else
+		echo >&2 "no such file: $1"
+		exit 1
+	fi
+fi
+
+
 echo "Enter a filename containing hex data or raw hex data to send it to the projector."
 ( while true; do
 	read -p '-> ' line
@@ -11,4 +25,4 @@ echo "Enter a filename containing hex data or raw hex data to send it to the pro
 	fi
 
 	echo "00: $line" | xxd -r
-done ) | nc 10.0.0.118 41794 | xxd
+done ) | nc $IP $PORT | xxd
